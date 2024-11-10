879c42fde8a9bc608d4e6dabd22c00a89e23ddeb
gabichulas <gabylopezromero13@gmail.com>
2024-11-10 17:46:21 -0300

    Grafiquitos

--- code/analysis.py
+++ code/analysis.py
@@ -22,7 +22,7 @@ df['q1_ms'] = df['q1'].apply(time_to_milliseconds)
 df['q2_ms'] = df['q2'].apply(time_to_milliseconds)
 df['q3_ms'] = df['q3'].apply(time_to_milliseconds)
 
-print(df[['q1_ms', 'q2_ms', 'q3_ms']].head(10))
+#print(df[['q1_ms', 'q2_ms', 'q3_ms']].head(10))
 
 # Eliminar valores atípicos en q1_ms ya que hay un dato muy grande
 Q1 = df['q1_ms'].quantile(0.25)
@@ -37,4 +37,30 @@ sns.boxplot(data=df_filtered[['q1_ms', 'q2_ms', 'q3_ms']])
 plt.xlabel('Sesión de Clasificación')
 plt.ylabel('Tiempo en Milisegundos')
 plt.savefig(r'images\laptimes_boxplot.png')
+plt.close()
+
+# A modo de experimentación, veremos los tiempos de vuelta del piloto con driverId = 4, o sea, Fernando Alonso
+df_alonso = df[df['driverId'] == 4]
+
+#print(df_hamilton.describe())
+
+# Importamos races, ya que necesitamos filtrar por alguna carrera para que los tiempos sean consistentes, ya que no todos los circuitos son iguales.
+# En este caso, buscaremos los id de las carreras que se corrieron en Silverstone.
+df_races = pd.read_csv(r"data\races.csv")
+df_races_filtered = df_races[df_races['name'] == "British Grand Prix"]['raceId'].values
+
+
+# A partir de este análisis, descubrimos que las carreras que se corrieron en Silverston fueron:
+df_alonso_silverstone = df_alonso[df_alonso['raceId'].isin(df_races_filtered)]
+print(df_alonso_silverstone)
+
+
+
+plt.figure(figsize=(10,6))
+sns.scatterplot(data=df_alonso_silverstone, x='position', y='q1_ms')
+sns.regplot(data=df_alonso_silverstone, x='position', y='q1_ms', scatter=False, color='red')
+plt.ylabel('Tiempo de Q1 en milisegundos')
+plt.xlabel('Posición final en la clasificación')
+plt.title("Relación entre Q1 y posición para Alonso en Silverstone")
+plt.savefig(r'images\Q1_position_alonso_silverstone.png')
 plt.close()
